36a7123ca777496740e9f5c38d382a891737cdab
afrolambo <stephennoahgalvan@gmail.com>
2021-02-22 00:43:09 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar></Navbar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
